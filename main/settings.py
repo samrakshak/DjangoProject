fd5bcde713b8053b6d1d3ac142c77cfa7859379d
samrakshak <karkisamrakshak@gmail.com>
2020-09-08 22:14:20 +0545

    changes

--- main/settings.py
+++ main/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 AUTH_USER_MODEL = 'club.User'
 
